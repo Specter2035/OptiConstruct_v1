fedeadb09f081ef1d339ddeb99c9fa1f8e16bffe
Specter2035 <carlos_.132@hotmail.com>
2026-04-26 19:09:33 -0600

    commit

--- MVC/routers/cliente.routers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ClienteController = require('../controllers/cliente.controller');
-router.get('/',ClienteController.obtenerClientes);// VER todos
-router.get('/:id',ClienteController.obtenerCliente);// uno por id
-router.post('/', ClienteController.crearCliente);///CREAR
-
-
-module.exports = router;
