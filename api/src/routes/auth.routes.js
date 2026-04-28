775b33e934aab59b54ff3178cd1e201d5e0036cc
Specter2035 <carlos_.132@hotmail.com>
2026-04-27 19:36:01 -0600

    commit

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const AuthController = require('../controllers/auth.controller');
-const { verificarToken} = require('../middleware/auth.middleware');
+const { verificarToken} = require('../middlewares/auth.middleware');
 
 router.post('/login', AuthController.login);
 router.get('/perfil', verificarToken, AuthController.perfil);
